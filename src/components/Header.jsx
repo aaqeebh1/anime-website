426671f39c6eedb08c450d9c9b50e7ae54d50cd9
Aaqeeb <24283167+aaqeebh1@users.noreply.github.com>
2022-08-31 15:45:26 +0100

    slow progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import './Header.css'
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Header </h1>
-        </div>
-    );
-}
+  return (
+    <div>
+     <h1></h1>
+    </div>
+  );
+};
 
 export default Header;
